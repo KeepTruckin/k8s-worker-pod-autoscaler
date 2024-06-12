657cc50f00902bd9b5fe266adc700838a4d1652c
Harsha Lokavarapu <harsha.lokavarapu@gomotive.com>
2024-06-12 12:30:49 -0700

    fix bugs

--- lib/loghelper.go
+++ lib/loghelper.go
@@ -2,15 +2,50 @@ package lib
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"time"
+
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 const (
+	DiscardLog int = iota
+	ConsoleLog
+	JSONLog
+
 	logMode  = "log-mode"
 	logLevel = "log-level"
 )
+
+func getOutput(logMode int) io.Writer {
+	switch logMode {
+	case DiscardLog:
+		return ioutil.Discard
+	case ConsoleLog:
+		return zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	case JSONLog:
+		return os.Stdout
+	default:
+		panic(fmt.Sprintf("%d is not a known log-mode", logMode))
+	}
+}
+
+// LogPrinter is used to satisfy the Printer interface on FX
+type LogPrinter struct {
+	Logger zerolog.Logger
+}
+
+func NewLogPrinter(logger zerolog.Logger) *LogPrinter {
+	return &LogPrinter{Logger: logger}
+}
+
+func (lp *LogPrinter) Printf(format string, args ...interface{}) {
+	lp.Logger.Info().Msgf(format, args...)
+}
 
 func NewLogger(cmd *cobra.Command, env string) (zerolog.Logger, error) {
 	var lm int
@@ -40,5 +75,5 @@ func NewLogger(cmd *cobra.Command, env string) (zerolog.Logger, error) {
 		return zerolog.Nop(), fmt.Errorf("error parsing the level %q: %s", slvl, err)
 	}
 
-	return NewLogger(lm).With().Str("env", env).Logger().Level(lvl), nil
+	return zerolog.New(getOutput(lm)).With().Timestamp().Logger().With().Str("env", env).Logger().Level(lvl), nil
 }
